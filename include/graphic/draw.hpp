27ef0fca05cfeb97acf8fe9af5a4983c247083c7
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2019-05-01 18:33:41 +0900

    cmkae update

--- include/graphic/draw.hpp
+++ include/graphic/draw.hpp
@@ -14,6 +14,7 @@ enum Color {
     BLUE,
     YELLOW,
     PURPLE,
+    ORANGE,
     WHITE,
     BLACK,
 };
--- src/graphic/draw.cpp
+++ src/graphic/draw.cpp
@@ -23,6 +23,7 @@ const std::vector<std::array<GLfloat, 3>> RGB
         {0.3f, 0.3f, 1.0f},
         {1.0f, 1.0f, 0.2f},
         {0.9f, 0.0f, 1.0f},
+        {1.0f, 0.7f, 0.1f},
         {1.0f, 1.0f, 1.0f},
         {0.0f, 0.0f, 0.0f},
 };
